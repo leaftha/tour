61d671e6f6a4e36b14ed46bdd629cac4592161b4
leaftha <vltpcks@gmail.com>
2025-01-16 12:55:24 +0900

    chat

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,6 +1,24 @@
+import { useState } from 'react';
 import style from './chat.module.css';
 
 const Chat = ({ setChatModal }) => {
+    var [listChat, setListChat] = useState([]);
+    var [currentPrompt, setCurrentPrompt] = useState('');
+    function generateText(prompt) {
+        if (prompt.includes('hello') || prompt.includes(' hi ')) {
+            return 'Hello, how can I help you?';
+        }
+        if (prompt.includes('help')) {
+            return 'What can I help you with?';
+        }
+        if (prompt.includes('korea')) {
+            return 'You can find guiders in Korea, especially me, to help you discovering the country :D!';
+        }
+        if (prompt.includes('viet')) {
+            return 'You can find guiders in Vietnam, especially me, to help you discovering the country :D!';
+        }
+        return "I can't understand what you mean, could you specify again?";
+    }
     return (
         <div className={style.main}>
             <div className={style.header}>
@@ -10,14 +28,29 @@ const Chat = ({ setChatModal }) => {
                 </p>
             </div>
             <div className={style.chatBody}>
-                <p>test1</p>
-                <p>test2</p>
-                <p>test3</p>
+                {listChat.map((item) => (
+                    <p>{item}</p>
+                ))}
             </div>
 
             <div className={style.inputComportent}>
-                <input className={style.input} />
-                <img src="/send-icon.png" alt="send btn img" />
+                <input
+                    className={style.input}
+                    onChange={(e) => {
+                        setCurrentPrompt(e.target.value);
+                        console.log('prompt: ', currentPrompt);
+                    }}
+                />
+                <img
+                    src="/send-icon.png"
+                    alt="send btn img"
+                    onClick={(e) => {
+                        const prompt = currentPrompt;
+                        setListChat([...listChat, prompt]);
+                        setListChat((prev) => [...prev, generateText(prompt)]);
+                        setCurrentPrompt('');
+                    }}
+                />
             </div>
         </div>
     );
